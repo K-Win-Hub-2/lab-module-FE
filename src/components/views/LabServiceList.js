d46e4babd8e317d9077420fa9db8a48b52ec0ba7
maymhuekoko <maymyatmoe211099@gmail.com>
2023-04-19 11:58:34 +0630

    test

--- src/components/views/LabServiceList.js
+++ src/components/views/LabServiceList.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import ExpenseDialog from "../views/ExpenseDialog";
+// import ExpenseDialog from "../views/ExpenseDialog";
 import { useEffect } from "react";
 import axios from "axios";
 import { ExcelExport } from "@progress/kendo-react-excel-export";
@@ -429,12 +429,12 @@ const excelExport = () => {
                 </div>
               </div>
 
-              <ExpenseDialog
+              {/* <ExpenseDialog
                 open={open}
                 close={() => setOpen(false)}
                 expenseLists={expenseLists}
                 setExpenseLists={setExpenseLists}
-              />
+              /> */}
             </div>{" "}
             {/*<!-- /.container-fluid --> */}
           </section>
