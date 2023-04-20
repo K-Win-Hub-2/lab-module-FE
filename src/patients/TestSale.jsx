465e07e3d4de70584fcbc078fdd9eba2d31f6d7c
maymhuekoko <maymyatmoe211099@gmail.com>
2023-04-20 15:42:14 +0630

    mmm

--- src/patients/TestSale.jsx
+++ src/patients/TestSale.jsx
@@ -59,7 +59,9 @@ const Option = styled.option``
 const TestSale = () => {
   const [pname,setPname] = useState('');
   const [pcode,setPcode] = useState('');
-  const [servs,setServs] = useState('');
+  const [servs,setServs] = useState([]);
+  const [arr,setArr] = useState([]);
+  const [serId,setSerId] = useState('');
   const [isDoctor,setIsDoctor] = useState(false);
   const [isEmail,setIsEmail] = useState(false);
   const patient_id = useLocation().pathname.split('/')[2];  
@@ -75,14 +77,25 @@ const TestSale = () => {
     const getServs = async () =>{
       try{
         const res = await axios.get('http://localhost:9000/api/services');
+        console.log('good')
         setServs(res.data.data);
       }catch(err){}
     }
     getPatient();
     getServs();
+    addService();
   },[]);
-
-
+  
+  const addService = async () => {
+    const res = await axios.get('http://localhost:9000/api/service/'+serId);
+    setArr( arr => [...arr, res.data.data]);
+  }
+  const delSer = (id) =>  {
+    setArr(arr.filter((el)=>el._id != id));
+  }
+  const saveTest = () => {
+    
+  }
   return (
     <div className="wrapper">
     <SideBar />
@@ -136,13 +149,17 @@ const TestSale = () => {
               <input type="email" className='form-control'/>
               </div>}
               <Div className='col-10 mt-5'>
-                <Select>
-                  <Option>Select Service</Option>
-                 
+                <Select onChange={(e)=>setSerId(e.target.value)} id='changes'>
+                  <Option value='0'>Select Service</Option>
+                 {
+                  servs.map((ser,index)=>(
+                    <Option value={ser._id}>{ser.name}</Option>
+                  ))
+                 }
                 </Select>
               </Div>
               <Div className='col-2 mt-5'>
-                  <Button>Add</Button>
+                  <Button onClick={addService}>Add</Button>
               </Div>
             </Div>
             <Table className='table table-hover mt-4'>
@@ -157,22 +174,15 @@ const TestSale = () => {
                 </Tr>
               </Thead>
               <Tbody>
-                <Tr>
+                { arr.map((ar,i)=>(
+                  <Tr key={ar._id}>
+                  <Td>{++i}</Td>
+                  <Td>{ar.name}</Td>
                   <Td>1</Td>
-                  <Td>Test1</Td>
-                  <Td>3</Td>
-                  <Td>3000</Td>
-                  <Td>9000</Td>
-                  <Td><RxCross2/></Td>
-                </Tr>
-                <Tr>
-                  <Td>2</Td>
-                  <Td>Test2</Td>
-                  <Td>4</Td>
-                  <Td>3000</Td>
-                  <Td>12000</Td>
-                  <Td><RxCross2/></Td>
-                </Tr>
+                  <Td>{ar.charges}</Td>
+                  <Td>{ar.charges}</Td>
+                  <Td><RxCross2 onClick={()=>delSer(ar._id)}/></Td>
+                </Tr> ))}
               </Tbody>
               <tfoot>
                 <Tr>
@@ -199,7 +209,7 @@ const TestSale = () => {
             </Table>
             <div className='row mt-5'>
                 <div className='offset-5 col-4'>
-                <button className='btn btn-sm btn-success'>Save</button>&nbsp;&nbsp;&nbsp;
+                <button className='btn btn-sm btn-success' onClick={saveTest}>Save</button>&nbsp;&nbsp;&nbsp;
                 <button className='btn btn-sm btn-primary'>Print</button>
                 </div>
             </div>
