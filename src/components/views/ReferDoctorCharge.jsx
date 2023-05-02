1f69614c5a02d6a15750e67e70692404d0f6197b
Kaung Set Hein <127381301+kaung-set107@users.noreply.github.com>
2023-05-02 21:29:40 +0630

    lab_module

--- src/components/views/ReferDoctorCharge.jsx
+++ src/components/views/ReferDoctorCharge.jsx
@@ -6,51 +6,61 @@ import Swal from "sweetalert2";
 
 function CatRegister() {
   const [referDoctor, setReferDoctor] = useState([]);
-  const DoctorID = useLocation().pathname.split('/')[2];
+  const DoctorID = useLocation().pathname.split("/")[2];
   const [open, setOpen] = useState(false);
   const [search, setSearch] = useState([]);
 
-  const handleCollect = () => {
-
-  }
+  const handleCollect = () => {};
 
   const handleSearch = (event) => {
-    axios.get('http://centralclinicbackend.kwintechnologykw11.com:3000/api/doctor/' + DoctorID).then(function (response) {
-      setSearch(response.data.data)
-    })
+    axios
+      .get(
+        "http://centralclinicbackend.kwintechnologykw11.com:3000/api/doctor/" +
+          DoctorID
+      )
+      .then(function (response) {
+        setSearch(response.data.data);
+      })
       .catch((err) => {
         Swal.fire({
           title: "Error",
           text: err.response.data.message,
           icon: "error",
           confirmButtonText: "CANCEL",
-        })
-      })
-  }
+        });
+      });
+  };
 
   const CollectDialog = () => {
     setOpen(true);
-  }
+  };
 
   useEffect(() => {
     const getPackage = async () => {
       try {
         const res = await axios.get(
           "http://centralclinicbackend.kwintechnologykw11.com:3000/api/doctor/" +
-          DoctorID
+            DoctorID
         );
 
         console.log(res.data.data.value);
         setReferDoctor(res.data.data);
-      } catch (err) { }
+      } catch (err) {}
     };
+
+    const search = async () => {
+      try {
+        const response = await axios.get(
+          "http://centralclinicbackend.kwintechnologykw11.com:3000/api/refer-commissions/doctor/64423be1b42471d6f4aa20a1?id=64423be1b42471d6f4aa20a1&date=Jan"
+        );
+        console.log("night");
+        console.log(response.data.data);
+      } catch (err) {}
+    };
+    search();
     getPackage();
   }, []);
-  const search = async () => {
-    const response = await axios.get('http://centralclinicbackend.kwintechnologykw11.com:3000/api/refer-commissions/doctor/64423be1b42471d6f4aa20a1?id=64423be1b42471d6f4aa20a1&date=Jan')
-  console.log('night');
-  console.log(response.data.data)
-  }
+  
   return (
     <div classNameName="App">
       {/* <!-- end preloader --> */}
@@ -96,57 +106,71 @@ function CatRegister() {
                     <div class="row" id="trial_balance">
                       <div className="offset-3 col-6  card">
                         <div class="card-body">
-                            <div className="row form-group">
-                              <div className="col-6">
-                                  <label htmlFor="">Monthly Name</label>
-                                  <select name="" id="" className="form-control">
-                                    <option value="">Choose Month</option>
-                                    <option value="Jan">Janaury</option>
-                                    <option value="Feb">Febuary</option>
-                                    <option value="Mar">March</option>
-                                    <option value="Apr">April</option>
-                                    <option value="May">May</option>
-                                    <option value="Jun">June</option>
-                                    <option value="July">July</option>
-                                    <option value="Aug">August</option>
-                                    <option value="Sep">September</option>
-                                    <option value="Oct">October</option>
-                                    <option value="Nov">November</option>
-                                    <option value="Dec">December</option>
-                                  </select>
-                              </div>
-                              <div className="col-4">
-                                  <button className="btn btn-m btn-info" style={{marginTop:'32px'}} onClick={search}>Search</button>
-                              </div>
-                              <div className="col-6 mt-3"> 
-                                <label htmlFor="">Collect Amount</label>
-                              </div>
-                              <div className="col-6 mt-3">
-                                <input type="number" className="form-control"/>
-                              </div>
-                              <div className="col-6 mt-3"> 
-                                <label htmlFor="">Status</label>
-                              </div>
-                              <div className="col-6 mt-3">
-                                <input type="text" className="form-control"/>
-                              </div>
-                              <div className="col-6 mt-3"> 
-                                <label htmlFor="">Collect Date</label>
-                              </div>
-                              <div className="col-6 mt-3">
-                                <input type="date" className="form-control"/>
-                              </div>
-                              <div className="col-6 mt-3"> 
-                                <label htmlFor="">Remark</label>
-                              </div>
-                              <div className="col-6 mt-3">
-                                <textarea name="" id="" cols="30" rows="3" className="form-control"></textarea>
-                              </div>
-                              <div className="offset-4 mt-5" >
-                                <button className="btn btn-sm btn-success">Collect</button>
-                                <button className="btn btn-sm btn-info ml-3">Detail</button>
-                              </div>
+                          <div className="row form-group">
+                            <div className="col-6">
+                              <label htmlFor="">Monthly Name</label>
+                              <select name="" id="" className="form-control">
+                                <option value="">Choose Month</option>
+                                <option value="Jan">Janaury</option>
+                                <option value="Feb">Febuary</option>
+                                <option value="Mar">March</option>
+                                <option value="Apr">April</option>
+                                <option value="May">May</option>
+                                <option value="Jun">June</option>
+                                <option value="July">July</option>
+                                <option value="Aug">August</option>
+                                <option value="Sep">September</option>
+                                <option value="Oct">October</option>
+                                <option value="Nov">November</option>
+                                <option value="Dec">December</option>
+                              </select>
                             </div>
+                            <div className="col-4">
+                              <button
+                                className="btn btn-m btn-info"
+                                style={{ marginTop: "32px" }}
+                                onClick={search}>
+                                Search
+                              </button>
+                            </div>
+                            <div className="col-6 mt-3">
+                              <label htmlFor="">Collect Amount</label>
+                            </div>
+                            <div className="col-6 mt-3">
+                              <input type="number" className="form-control" />
+                            </div>
+                            <div className="col-6 mt-3">
+                              <label htmlFor="">Status</label>
+                            </div>
+                            <div className="col-6 mt-3">
+                              <input type="text" className="form-control" />
+                            </div>
+                            <div className="col-6 mt-3">
+                              <label htmlFor="">Collect Date</label>
+                            </div>
+                            <div className="col-6 mt-3">
+                              <input type="date" className="form-control" />
+                            </div>
+                            <div className="col-6 mt-3">
+                              <label htmlFor="">Remark</label>
+                            </div>
+                            <div className="col-6 mt-3">
+                              <textarea
+                                name=""
+                                id=""
+                                cols="30"
+                                rows="3"
+                                className="form-control"></textarea>
+                            </div>
+                            <div className="offset-4 mt-5">
+                              <button className="btn btn-sm btn-success">
+                                Collect
+                              </button>
+                              <button className="btn btn-sm btn-info ml-3">
+                                Detail
+                              </button>
+                            </div>
+                          </div>
                         </div>
                       </div>
                     </div>
